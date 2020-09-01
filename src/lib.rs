f8a256d06fd3e6467da7d335d70783fde4c505c6
Juan Jose de Orus <juan@uberresearch.com>
2020-09-01 18:31:37 +0200

    SearchParam subtypes as Enum.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// TODO: Reqwest is curretly configured in blokcing mode. To support both blocking and
+// TODO: Reqwest is curretly configured in blocking mode. To support both blocking and
 // non-blocking modes one needs to use conditional complilation ?
 use std::iter::IntoIterator;
 use std::env::consts::OS;
@@ -6,6 +6,8 @@ use std::error::Error;
 use reqwest::blocking::{Client, Response};
 use reqwest::header;
 use url::{Url, ParseError};
+// use serde::ser::{Serialize, Serializer};
+
 
 // Builder --------------------------------------------------------------------
 
@@ -119,6 +121,8 @@ pub mod taxonomy {
   taxonomy!(INGREDIENT_ANALYSIS, "ingredients-analysis");  // Note the '-'
   taxonomy!(LANGUAGES, "languages");
   taxonomy!(NOVA_GROUPS, "nova_groups");
+  // TODO: Use nutrient or nutriment names consistently.
+  taxonomy!(NUTRIMENTS, "nutrients");
   taxonomy!(NUTRIENT_LEVELS, "nutrient_levels");
   taxonomy!(PRODUCT_STATES, "states");
 }
@@ -145,37 +149,6 @@ pub mod facet {
 
 // Search criteria names
 pub mod criteria {
-  // https://wiki.openfoodfacts.org/API/Read/Search
-  // search_terms: list of terms
-  // format: Always JSON
-  // page_size: usize
-  // sort_by: String or Enum (Popularity, Product name, Add date, Edit date, Completness)
-  // tags : A collection of tuples (name, op, value) ? -> tag_0 ... tag_N
-  // nutrients: A collection of tuples (name, op, value) ? -> nutriment_0 ... nutriment_N
-  // Zero or more pairs (name, value)
-  // All names parameters search_terms .. nutrient0 can in fact be given using the pair
-  // form.
-
-  // A search consists of one or more search criteria, defined as a triplet (name, contains, value)
-  // where are are strings. <name> can be any values of tag_type. contains is a boolean and value
-  // is a string.
-  //
-  // Criteria are "serialized" into query parameters as:
-  //
-  //  tagtype_<n>=<name>&tag_contains_<n>=<contains>&tag_<n>=<value>
-  //
-  // where <n> is the index of the criteria in the criteria list (starts at 0).
-  //
-  // TODO: Could be changed to
-  //
-  // Criteria(CriteriaName, op, value)
-  //
-  //  - where CriteriaName and op are Enums
-  //
-  // OR
-  // 
-  // Criteria(CriteriaName).op(value)
-  //
   pub struct Criteria(pub &'static str);
 
   macro_rules! criteria {
@@ -199,8 +172,19 @@ pub mod criteria {
   criteria!(STATES, "states");
 }
 
-// Search param types
+// Search parameter types
 
+// Search criteria parameters. Each parameter serializes to the triplet
+//  tagtype_N=<name>
+//  tag_contains_N=<op>
+//  tag_N=<value>
+//
+// The index N is given by the SearchParams serializer and indicates the
+// number of this CriteriaParam in the SearchParams vector.
+//
+// name: Must be one of the criteria::NAME values.
+// op: One of "contains" or "does_not_contain".
+// value: a string with the searched criteria value.
 #[derive(Debug)]
 pub struct CriteriaParam {
   name: String,
@@ -208,12 +192,33 @@ pub struct CriteriaParam {
   value: String
 }
 
+// Ingredient parameters. Each parameter serializes to a pair
+//  <name>=<value>
+//
+// <name>: Any of the following: "additives", "ingredients_from_palm_oil",
+// "ingredients_that_may_be_from_palm_oil", "ingredients_from_or_that_may_be_from_palm_oil"
+// <value>: Any of the following: "with", "without", "indifferent".
+//
+// If <name> is "additives", the values are converted respectively to "with_additives",
+// "without_additives" and "indifferent_additives".
 #[derive(Debug)]
 pub struct IngredientParam {
   name: String,
   value: String
 }
 
+// Nutriment parameters. Each parameter serializes to the triplet
+//  nutriment_N=<name>
+//  nutriment_compare_N=<op>
+//  nutriment_value_N=<value>
+//
+// The index N is given by the SearchParams serializer and indicates the
+// number of this NutrimentParam in the SearchParams vector.
+//
+// name: Must be one of the nutriment values returned by the taxonomxy::NUTRIMENTS
+//  taxonomy.
+// op: One of "lt", "lte", "gt", "gte" or "eq".
+// value: a string with the searched nutriment value.
 #[derive(Debug)]
 pub struct NutrimentParam {
   name: String,
@@ -221,6 +226,11 @@ pub struct NutrimentParam {
   value: String
 }
 
+
+// Product name search parameter. Serializes to
+// product_name=<name>
+//
+// <name>: Any valid product name.
 #[derive(Debug)]
 pub struct NameParam {
   name: String,
@@ -235,27 +245,25 @@ pub enum SearchParam {
   Name(NameParam)
 }
 
-// TODO: Implemenrt using Traits. All search param types will support Serde serialization into
-// query strings.
-// In order to be stored into the Vector, we need a trait represeting the common type.
-
-// Types:
-//  - CriteriaParam (name, op, value)
-//  - IngredientParam (name, value)
-//  - NutrimentParam (name, op, value)
-//  - NameParam (name)
-// All types implement the serde_urlencoded::Serializer trait
+// TODO:
+// format: Default is JSON
+// page and page_size
+// sort_by: String or Enum (Popularity, Product name, Add date, Edit date, Completness)
+// created_t
+// last_modified_t
+// tags : A collection of tuples (name, op, value) ? -> tag_0 ... tag_N
+// unique_scans_N : ??
 
 
 pub struct SearchParams(Vec<SearchParam>);
 
-// Builder for seartch params. Produces a collection of objects supporting serialization.
+// Builder for search params. Produces a collection of objects supporting serialization.
 impl SearchParams {
   pub fn new() -> Self {
     Self(Vec::new())
   }
 
-  pub fn criteria(& mut self, criteria: &criteria::Criteria, op: &str, value: &str) -> &Self {
+  pub fn criteria(& mut self, criteria: &criteria::Criteria, op: &str, value: &str) -> & mut Self {
     self.0.push(SearchParam::Criteria(CriteriaParam {
       name: String::from(criteria.0),
       op: String::from(op),
@@ -264,7 +272,7 @@ impl SearchParams {
     self
   }
 
-  pub fn ingredient(& mut self, ingredient: &str, value: &str) -> &Self {
+  pub fn ingredient(& mut self, ingredient: &str, value: &str) -> & mut Self {
     self.0.push(SearchParam::Ingredient(IngredientParam {
       name: String::from(ingredient),
       value: String::from(value)
@@ -272,7 +280,7 @@ impl SearchParams {
     self
   }
 
-  pub fn nutriment(& mut self, nutriment: &str, op: &str, value: usize) -> &Self {
+  pub fn nutriment(& mut self, nutriment: &str, op: &str, value: usize) -> & mut Self {
     self.0.push(SearchParam::Nutriment(NutrimentParam {
       name: String::from(nutriment),
       op: String::from(op),
@@ -281,7 +289,7 @@ impl SearchParams {
     self
   }
 
-  pub fn name(&mut self, name: &str) -> &Self {
+  pub fn name(&mut self, name: &str) -> & mut Self {
     self.0.push(SearchParam::Name(NameParam {
       name: String::from(name),
     }));
@@ -297,6 +305,20 @@ impl IntoIterator for SearchParams {
     self.0.into_iter()
   }
 }
+
+
+// TODO: Use UrlEncodedSerializer ?
+// impl Serialize for SearchParams {
+//   fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+//   where
+//       S: Serializer,
+//   {
+//       // The simplest implementation is to convert each SeachParam in an array of
+//       // pairs name=value
+//       // and call serde_urlencoded::to_string() on it.
+//       Ok("name=value")
+//   }
+// }
 
 
 pub struct Off {
@@ -509,13 +531,13 @@ mod tests {
   #[test]
   fn test_off_search_params() {
     let mut search_params = SearchParams::new();
-    search_params.criteria(&criteria::BRANDS, "contains", "Nestlé");
-    search_params.criteria(&criteria::CATEGORIES, "does_not_contain", "cheese");
-    search_params.ingredient("additives", "without");
-    search_params.ingredient("ingredients_that_may_be_from_palm_oil", "indifferent");
-    search_params.nutriment("fiber", "lt", 500);
-    search_params.nutriment("salt", "gt", 100);
-    search_params.name("name");
+    search_params.criteria(&criteria::BRANDS, "contains", "Nestlé")
+      .criteria(&criteria::CATEGORIES, "does_not_contain", "cheese")
+      .ingredient("additives", "without")
+      .ingredient("ingredients_that_may_be_from_palm_oil", "indifferent")
+      .nutriment("fiber", "lt", 500)
+      .nutriment("salt", "gt", 100)
+      .name("name");
     // Other: sort by, , product_name, created, last_modified,
     // builder.sort_by();
     // builder.created(date);
@@ -529,33 +551,44 @@ mod tests {
 
     let mut spi = search_params.into_iter();
   
-    let brands = spi.next().unwrap();
-    assert_eq!(brands.name, criteria::BRANDS.0);
-    assert_eq!(brands.op, "contains");
-    assert_eq!(brands.value, "Nestlé");
+    if let SearchParam::Criteria(brands) = spi.next().unwrap() {
+      assert_eq!(brands.name, criteria::BRANDS.0);
+      assert_eq!(brands.op, "contains");
+      assert_eq!(brands.value, "Nestlé");
+    }
+    else {
+      panic!("Not a CriteriaParam")
+    }
 
     spi.next();   // CATEGORIES
 
-    let ingredient = spi.next().unwrap();
-    assert_eq!(ingredient.name, "additives");
-    assert_eq!(ingredient.op, "");
-    assert_eq!(ingredient.value, "without");
+    if let SearchParam::Ingredient(ingredient) = spi.next().unwrap() {
+      assert_eq!(ingredient.name, "additives");
+      assert_eq!(ingredient.value, "without");
+    }
+    else {
+      panic!("Not an Ingredient")
+    }
 
     spi.next();   // ingredients_that_may_be_from_palm_oil
 
-    let nutriment = spi.next().unwrap();
-    assert_eq!(nutriment.name, "fiber");
-    assert_eq!(nutriment.op, "lt");
-    assert_eq!(nutriment.value, "500");
+    if let SearchParam::Nutriment(nutriment) = spi.next().unwrap() {
+      assert_eq!(nutriment.name, "fiber");
+      assert_eq!(nutriment.op, "lt");
+      assert_eq!(nutriment.value, "500");
+    }
+    else {
+      panic!("Not an Nutriment")
+    }
 
     spi.next();   // salt
 
-    let product_name = spi.next().unwrap();
-    assert_eq!(product_name.name, "name");
-    assert_eq!(product_name.op, "");
-    assert_eq!(product_name.value, "");
-
-
+    if let SearchParam::Name(product_name) = spi.next().unwrap() {
+      assert_eq!(product_name.name, "name");
+    }
+    else {
+      panic!("Not an Name")
+    }
   }
 
   // Use/keep as example.
